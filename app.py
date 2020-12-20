8c39bf3bfb28836fbbe42dccb43ad48356cd8527
Ryohei <o.ryohei07@gmail.com>
2020-12-20 20:39:01 +0900

    add transition

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 from flask import Flask, render_template
@@ -12,12 +12,30 @@ app=Flask(__name__)
 def omikuji():
     event = list(["あたり","はずれ"])
     return random.choice(event)
+
 @app.route('/')
+def start():
+        return """
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <title>おみくじ</title>
+        </head>
+        <body>
+            おみくじ開始</br>
+            <form action="/result" method="GET">
+                <input type="submit" value="開始" >
+            </form>
+        </body>
+        </html>"""
+    
+@app.route('/result')
 def index():
     result = omikuji()
     #テンプレートエンジンにデータを指定
     return render_template("index.html",
                           result = result)
+
 if __name__ =="__main__":
     app.run()
 
